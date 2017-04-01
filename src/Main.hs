1780cb539341a01acd43fa64851be5a6dc2d007b
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-04-01 23:04:18 +0300

    Use LLVM output file instead of REPL after compilation

--- src/Main.hs
+++ src/Main.hs
@@ -5,9 +5,11 @@ import Codegen
 
 import Control.Monad.Trans
 import System.Console.Haskeline
+import Data.Maybe
+import System.Process
 
-printBlocks :: Codegen -> String
-printBlocks codegen =
+serializeBlocks :: Codegen -> String
+serializeBlocks codegen =
   globalBlock codegen ++
   concat (funcBlocks codegen) ++
   mainBlock codegen
@@ -17,13 +19,16 @@ process line = do
   let res = parseToplevel line
   case res of
     Left err -> print err
-    Right ex -> putStrLn . printBlocks $ codegen ex
+    Right ex -> do
+      putStrLn code
+      writeFile "out.ll" code
+      createProcess (proc "lli" ["out.ll"])
+      return ()
+      where code = serializeBlocks (codegen ex)
 
 main :: IO ()
 main = runInputT defaultSettings loop
   where
   loop = do
-    minput <- getInputLine "ready> "
-    case minput of
-      Nothing -> outputStrLn "Goodbye."
-      Just input -> liftIO (process input) >> loop
+    minput <- liftIO $ readFile "in.gl"
+    liftIO (process minput) -- >> loop
